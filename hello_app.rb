1e23ab984f0e7bb6c4fdc97755ff6dcd8c9e2a02
Warren Smyth <warrensmyth@me.com>
2020-12-10 14:17:42 -0600

    Move dayname constant outside of get

--- hello_app.rb
+++ hello_app.rb
@@ -1,8 +1,9 @@
 require 'sinatra'
 
-get '/' do
-	DAYNAMES = ["Sunday", "Monday", "Tuesday", "Wednesday",
+DAYNAMES = ["Sunday", "Monday", "Tuesday", "Wednesday",
 							"Thursday", "Friday", "Saturday"]
+get '/' do
+	
 	dayname = DAYNAMES[Time.now.wday]
   "Hello, world! Happy #{dayname}."
 end
