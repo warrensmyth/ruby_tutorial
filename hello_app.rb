24272834952fe47c7a3dd6142dcf9ca12d9371e3
Warren Smyth <warrensmyth@me.com>
2020-12-11 12:10:59 -0600

    Clean up hello_app by moving functions to separate file and writing require

--- hello_app.rb
+++ hello_app.rb
@@ -1,6 +1,6 @@
 require 'sinatra'
+require './day'
 
 get '/' do
-	dayname = Date::DAYNAMES[Time.now.wday]
-  "Hello, world! Happy #{dayname}."
+  "Hello, world! Happy #{day_of_the_week(Time.now)}-now from a file!"
 end
